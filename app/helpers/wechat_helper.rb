33cc6c199c3204fce22249180346a639543c3e30
destinyd <destinyd_war@163.com>
2016-09-20 17:39:30 +0800

    全局配置，是否使用微信自动登录

--- app/helpers/wechat_helper.rb
+++ app/helpers/wechat_helper.rb
@@ -1,6 +1,7 @@
 module WechatHelper
   def wechat_login
-    # guest_user也算登录
+    # 如果配置设置不自动登录，则忽略
+    return true if ENV['WECHAT_AUTO_LOGIN'] == 'false'
     # 未登录，且未记录微信，则验证
     return true if user_signed_in?
     return true unless request.get?
